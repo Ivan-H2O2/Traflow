8b485b3c66b7eba4ec2aa5c608abe87c267d5239
28H2O2 <hcy1111hcy@sina.com>
2023-01-20 16:20:41 +0800

    project

---
+++ traflow/vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link>
+  </nav> -->
+  <div>
+    <!-- 主体 -->
+    <Header />
+    <div style="display: flex">
+      <!-- 侧边栏 -->
+      <Aside />
+      <!-- 内容区域 -->
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+export default {
+  name: "LayOut",
+  components: {
+    Header,
+    Aside,
+  },
+};
+</script>
---
+++ traflow/vue/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="padding: 10px">
+    <el-button type="primary">Hello</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "HomeView",
+};
+</script>
